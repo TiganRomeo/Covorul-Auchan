2d491e7f36f9506d0bc843e886dad8364b227d58
Tigan Romeo Paul <102172218+TiganRomeo@users.noreply.github.com>
2023-02-21 02:43:47 +0200

    Second homepage redesign

--- core/templatetags/category_template_tags.py
+++ core/templatetags/category_template_tags.py
@@ -49,7 +49,7 @@ def categories_div():
         else:
             items_div_ = """<div class="block1 hov-img-zoom pos-relative m-b-30"><img src="/media/{}" alt="IMG-BENNER"><div class="block1-wrapbtn w-size2"><a href="/category/{}" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">{}</a></div></div>""".format(
                 j.image, j.slug, j.title)
-            item_div_list += """<div class="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">""" + items_div + items_div_ + """</div>"""
+            item_div_list += """<div class="col-sm-10 col-md-8 col-lg-5 m-l-r-auto">""" + items_div + items_div_ + """</div>"""
             items_div = ""
 
     return mark_safe(item_div_list)
